19f2fbdb0c986fce5bdc3d458e3d3ea8ce87932e
vicLim88 <vic.lim@icloud.com>
2023-09-09 14:34:50 +0800

    changed init file at src

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
-from .file_manager.public.file_manager_factory import FileManagerFactory
-from .file_manager.public.file_manager_base import file_manager_base
+from file_manager.public.file_manager_factory import FileManagerFactory
+from file_manager.public.file_manager_base import file_manager_base
