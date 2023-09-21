0c2c2cb05d012b603c2289e48ad711185afc7a92
vicLim88 <vic.lim@icloud.com>
2023-09-21 14:57:25 +0800

    Repackaged to set the version to 3.10

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from vic_lim_wx.file_manager.public.file_manager_base import FileManagerBase
-from vic_lim_wx.file_manager.public.file_manager_factory import \
+from .vic_lim_wx.file_manager.public.file_manager_base import FileManagerBase
+from .vic_lim_wx.file_manager.public.file_manager_factory import \
     FileManagerFactory
--- vic_lim_wx/__init__.py
+++ vic_lim_wx/__init__.py
@@ -1,2 +1,2 @@
-from file_manager.public.file_manager_factory import FileManagerFactory
-from file_manager.public.file_manager_base import FileManagerBase
+from .file_manager.public.file_manager_factory import FileManagerFactory
+from .file_manager.public.file_manager_base import FileManagerBase
