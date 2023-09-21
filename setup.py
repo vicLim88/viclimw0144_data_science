c9166e0923707f24330822463ce0e13e9d8fc97b
vicLim88 <vic.lim@icloud.com>
2023-09-21 13:16:25 +0800

    Repackaged to set the version to 3.10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.2.7'
+VERSION = '0.2.8'
 DESCRIPTION = 'Manages data by converting them to dataframes'
 LONG_DESCRIPTION = ''' A package that provides a convenient way to manage and 
     manipulate different types of data sources and convert them to Pandas 
